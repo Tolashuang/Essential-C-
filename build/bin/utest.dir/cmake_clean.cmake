1fd292f850cb7d6a08d18de0358a1e757e6395a9
Toalshuang <276255565@qq.com>
2020-03-07 06:51:56 -0800

    saturday

---
+++ build/bin/utest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/../bin/utest.dir/Chapter1/Q&A1.5.cpp.o"
+  "../bin/utest.pdb"
+  "../bin/utest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/../bin/utest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
